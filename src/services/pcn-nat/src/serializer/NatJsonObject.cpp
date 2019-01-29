65026d6713320f8a69c57b92a4e1c79e2e1325c3
Fylax <nico.caprioli@gmail.com>
2019-01-29 20:57:34 +0100

    Ported nat
    Signed-off-by: Nico Caprioli <nico.caprioli@gmail.com>

--- src/services/pcn-nat/src/serializer/NatJsonObject.cpp
+++ src/services/pcn-nat/src/serializer/NatJsonObject.cpp
@@ -22,53 +22,56 @@ namespace swagger {
 namespace server {
 namespace model {
 
-NatJsonObject::NatJsonObject() {
+NatJsonObject::NatJsonObject() :
+  m_nameIsSet (false),
+  m_uuidIsSet (false),
+  m_type (CubeType::TC),
+  m_typeIsSet (true),
+  m_loglevel (NatLoglevelEnum::INFO),
+  m_loglevelIsSet (true),
+  m_ruleIsSet (false),
+  m_nattingTableIsSet (false) { }
 
-  m_nameIsSet = false;
+NatJsonObject::NatJsonObject(nlohmann::json& val) :
+  m_nameIsSet (false),
+  m_uuidIsSet (false),
+  // Item with a default value, granted to be part of the request body
+  m_type (string_to_CubeType(val.at("type").get<std::string>())),
+  m_typeIsSet (true),
+  // Item with a default value, granted to be part of the request body
+  m_loglevel (string_to_NatLoglevelEnum(val.at("loglevel").get<std::string>())),
+  m_loglevelIsSet (true),
+  m_ruleIsSet (false),
+  m_nattingTableIsSet (false) {
 
-  m_uuidIsSet = false;
-
-  m_type = CubeType::TC;
-  m_typeIsSet = false;
-
-  m_loglevel = NatLoglevelEnum::INFO;
-  m_loglevelIsSet = false;
-
-  m_ruleIsSet = false;
-
-  m_nattingTableIsSet = false;
-}
-
-NatJsonObject::~NatJsonObject() {}
-
-void NatJsonObject::validateKeys() {
-
-  if (!m_nameIsSet) {
-    throw std::runtime_error("Variable name is required");
+  if (val.count("uuid") != 0) {
+    setUuid(val.at("uuid").get<std::string>());
   }
-}
 
-void NatJsonObject::validateMandatoryFields() {
 
-}
 
-void NatJsonObject::validateParams() {
+  if (val.count("rule") != 0) {
 
-  if (m_uuidIsSet) {
-    std::string patter_value = R"PATTERN([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})PATTERN";
-    std::regex e (patter_value);
-    if (!std::regex_match(m_uuid, e))
-      throw std::runtime_error("Variable uuid has not a valid format");
+
+    if (!val["rule"].is_null()) {
+      RuleJsonObject newItem { val["rule"] };
+      setRule(newItem);
+    }
   }
+
+  m_nattingTable.clear();
+  for (auto& item : val["natting-table"]) {
+    NattingTableJsonObject newItem { item };
+    m_nattingTable.push_back(newItem);
+  }
+  m_nattingTableIsSet = !m_nattingTable.empty();
+
 }
 
 nlohmann::json NatJsonObject::toJson() const {
   nlohmann::json val = nlohmann::json::object();
 
-  if (m_nameIsSet) {
-    val["name"] = m_name;
-  }
-
+  val["name"] = m_name;
   if (m_uuidIsSet) {
     val["uuid"] = m_uuid;
   }
@@ -96,54 +99,6 @@ nlohmann::json NatJsonObject::toJson() const {
   }
 
   return val;
-}
-
-void NatJsonObject::fromJson(nlohmann::json& val) {
-  for(nlohmann::json::iterator it = val.begin(); it != val.end(); ++it) {
-    std::string key = it.key();
-    bool found = (std::find(allowedParameters_.begin(), allowedParameters_.end(), key) != allowedParameters_.end());
-    if (!found) {
-      throw std::runtime_error(key + " is not a valid parameter");
-      return;
-    }
-  }
-
-  if (val.find("name") != val.end()) {
-    setName(val.at("name"));
-  }
-
-  if (val.find("uuid") != val.end()) {
-    setUuid(val.at("uuid"));
-  }
-
-  if (val.find("type") != val.end()) {
-    setType(string_to_CubeType(val.at("type")));
-  }
-
-  if (val.find("loglevel") != val.end()) {
-    setLoglevel(string_to_NatLoglevelEnum(val.at("loglevel")));
-  }
-
-
-  if (val.find("rule") != val.end()) {
-
-
-    if (!val["rule"].is_null()) {
-      RuleJsonObject newItem;
-      newItem.fromJson(val["rule"]);
-      setRule(newItem);
-    }
-  }
-
-  m_nattingTable.clear();
-  for (auto& item : val["natting-table"]) {
-
-    NattingTableJsonObject newItem;
-    newItem.fromJson(item);
-    m_nattingTable.push_back(newItem);
-    m_nattingTableIsSet = true;
-  }
-
 }
 
 nlohmann::json NatJsonObject::helpKeys() {
@@ -233,9 +188,7 @@ bool NatJsonObject::nameIsSet() const {
   return m_nameIsSet;
 }
 
-void NatJsonObject::unsetName() {
-  m_nameIsSet = false;
-}
+
 
 
 
@@ -278,22 +231,22 @@ void NatJsonObject::unsetType() {
 std::string NatJsonObject::CubeType_to_string(const CubeType &value){
   switch(value){
     case CubeType::TC:
-      return std::string("TC");
+      return std::string("tc");
     case CubeType::XDP_SKB:
-      return std::string("XDP_SKB");
+      return std::string("xdp_skb");
     case CubeType::XDP_DRV:
-      return std::string("XDP_DRV");
+      return std::string("xdp_drv");
     default:
       throw std::runtime_error("Bad Nat type");
   }
 }
 
 CubeType NatJsonObject::string_to_CubeType(const std::string &str){
-  if (JsonObjectBase::iequals("TC", str))
+  if (JsonObjectBase::iequals("tc", str))
     return CubeType::TC;
-  if (JsonObjectBase::iequals("XDP_SKB", str))
+  if (JsonObjectBase::iequals("xdp_skb", str))
     return CubeType::XDP_SKB;
-  if (JsonObjectBase::iequals("XDP_DRV", str))
+  if (JsonObjectBase::iequals("xdp_drv", str))
     return CubeType::XDP_DRV;
   throw std::runtime_error("Nat type is invalid");
 }
@@ -417,4 +370,5 @@ void NatJsonObject::unsetNattingTable() {
 }
 }
 }
+
 
