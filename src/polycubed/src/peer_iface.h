10cda9ee64ee78064db7271a828ab7d2c56177b2
Riccardo Marchi <riccardo.marchi4@gmail.com>
2019-05-08 02:41:29 +0200

    load topology at startup and save each modification to memory and file
    This feature allows to load a topology from file at daemon startup,
    either from the default folder or from a user selected file, like
    
    polycubed --cubes-dump ~/Desktop/cubes.yaml
    
    keeping the whole configuration in memory.
    To enable this feature we need to use the flag --cubes-dump-enable.
    After each modification in any of the topology of the cube,
    the configuration in memory is updated and another thread saves it
    to file updating it too.
    It is possible to start the daemon with an empty topology
    by using the flag --cubes-init: if the file contains a topology,
    at the first update it is overwritten.
    
    Signed-off-by: Riccardo Marchi <riccardo.marchi4@gmail.com>

--- src/polycubed/src/peer_iface.h
+++ src/polycubed/src/peer_iface.h
@@ -48,7 +48,7 @@ class PeerIface {
   virtual void set_parameter(const std::string &param_name,
                              const std::string &value) = 0;
 
-  void add_cube(TransparentCube *cube, const std::string &position,
+  void add_cube(TransparentCube *cube, std::string *position,
                 const std::string &other);
   void remove_cube(const std::string &type);
   std::vector<std::string> get_cubes_names() const;
