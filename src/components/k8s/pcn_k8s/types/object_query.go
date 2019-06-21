dc6a21f431d5075bc4da5812a2b519ded278ea21
Elis <elis.lulja@gmail.com>
2019-06-21 11:40:49 +0200

    pcn-k8s: add object query type
    The object query is a struct that contains information on how to get objects from controllers.
    By can be either "name" or "labels".
    The appropriate fields (name, labels) must be filled.
    
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

---
+++ src/components/k8s/pcn_k8s/types/object_query.go
@@ -0,0 +1,9 @@
+package types
+
+// ObjectQuery is a struct that specifies how the object should be found
+type ObjectQuery struct {
+	By     string
+	Name   string
+	Labels map[string]string
+	Node   string
+}
