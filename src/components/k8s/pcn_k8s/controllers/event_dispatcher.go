89ebf220bb343bbed580cfa6fa8e728907b3bad1
Elis <elis.lulja@gmail.com>
2019-06-21 12:00:40 +0200

    pcn-k8s: create the event dispatcher
    The event dispatcher holds function pointers that are going to be executed when an event occurs.
    When that event is triggered, the event is dispatched: it will loop through all functions and execute them.
    
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

---
+++ src/components/k8s/pcn_k8s/controllers/event_dispatcher.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+	"sync"
+
+	pcn_types "github.com/SunSince90/polycube/src/components/k8s/pcn_k8s/types"
+)
+
+// EventDispatcher dispatches the event to all subscribers
+type EventDispatcher struct {
+	name        string
+	n           pcn_types.SubscriberID
+	subscribers map[pcn_types.SubscriberID]subscriber
+	lock        sync.Mutex
+}
+
+// EventDispatchersContainer contains the three basic EventDispatcher-s: New, Update, Delete
+type EventDispatchersContainer struct {
+	new    *EventDispatcher
+	update *EventDispatcher
+	delete *EventDispatcher
+}
+
+type subscriber func(interface{})
+
+// NewEventDispatcher starts a new event dispatcher
+func NewEventDispatcher(name string) *EventDispatcher {
+	return &EventDispatcher{
+		name:        name,
+		n:           0,
+		subscribers: make(map[pcn_types.SubscriberID]subscriber),
+	}
+}
+
+// Dispatch will dispatch the event to the list of subscribers
+func (d *EventDispatcher) Dispatch(item interface{}) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	//	Are there any subscribers?
+	if (len(d.subscribers)) < 1 {
+		return
+	}
+
+	//	Loop through all of the subscribers
+	for _, s := range d.subscribers {
+		// The controller will make it go
+		//	go s(item)
+		s(item)
+	}
+}
+
+// Add will add a new subscriber
+func (d *EventDispatcher) Add(s subscriber) pcn_types.SubscriberID {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	d.subscribers[d.n+1] = s
+	d.n++
+
+	return d.n
+}
+
+// Remove will remove a subscriber
+func (d *EventDispatcher) Remove(i pcn_types.SubscriberID) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	if _, exists := d.subscribers[i]; exists {
+		delete(d.subscribers, i)
+	}
+
+}
+
+// CleanUp will remove all subscribers at once
+func (d *EventDispatcher) CleanUp() {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	//	The suggested way to clean up a map, is to create a new empty one
+	//	The garbage collector will take care of the rest
+	d.subscribers = make(map[pcn_types.SubscriberID]subscriber)
+
+}
---
+++ src/components/k8s/pcn_k8s/controllers/utils.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"strings"
+
+	pcn_types "github.com/SunSince90/polycube/src/components/k8s/pcn_k8s/types"
+	"k8s.io/client-go/tools/cache"
+)
+
+// buildEvent builds the event
+func buildEvent(obj interface{}, eventType pcn_types.EventType) (pcn_types.Event, error) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		return pcn_types.Event{}, err
+	}
+	namespace, _, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return pcn_types.Event{}, err
+	}
+
+	event := pcn_types.Event{
+		Key:       key,
+		Type:      eventType,
+		Namespace: namespace,
+		Object:    obj,
+	}
+
+	return event, nil
+}
+
+// implodeLabels set labels in a key1=value1,key2=value2 format
+func implodeLabels(labels map[string]string) string {
+	implodedLabels := ""
+
+	for k, v := range labels {
+		implodedLabels += k + "=" + v + ","
+	}
+
+	return strings.Trim(implodedLabels, ",")
+}
